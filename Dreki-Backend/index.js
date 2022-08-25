6781d6d4cb10d7a18bcb38055a207748ddaa8aff
xxORVARxx <xxORVARxx@gmail.com>
2022-08-25 21:50:58 +0000

    Fixing code.

--- Dreki-Backend/index.js
+++ Dreki-Backend/index.js
@@ -1,55 +1,15 @@
 "use strict";
-const nodemailer = require("nodemailer");
 
 require('dotenv').config();
+require("nodemailer");
+
 const { initializeApp, applicationDefault } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
-
-
-
 initializeApp({
   credential: applicationDefault(), // This will look for the credential at the default location.
 });
 console.log( process.env.GOOGLE_APPLICATION_CREDENTIALS ); // This is the default location for the credential.
-
 const db = getFirestore();
-
-
-
-async function fire(){
-  console.log('Do Fire!');
-  return true;
-}
-
-
-
-const doc = db.collection('fire');
-const unsub = doc.onSnapshot(docSnapshot => {
-  // Callback function.
-  console.log('Received doc snapshot.');
-  const batch = db.batch();
-
-  // The 'every()' function behaves exactly like 'forEach()', except that...
-  // returning false is equivalent to a 'break', and returning true is equivalent to a 'continue'.
-  docSnapshot.forEach(function(doc) {
-    const fireID = doc.id;
-    const code = doc.data().code;
-    const age_sec = docSnapshot.readTime.seconds - doc.data().timestamp.seconds;
-
-    if(age_sec < 90){
-      console.log('Fire ID:', fireID);
-      if(fire()){
-        db.collection('permits').doc(code).delete();
-      }
-    }
-    batch.delete(doc.ref);
-    
-  });
-
-  batch.commit();
-}, err => {
-  console.log(`Encountered error: ${err}`);
-});
 
 
 
@@ -66,12 +26,39 @@ process.on('SIGINT', exitHandler);
 
 
 
-/*
-async function get_firestore(){
-  const querySnapshot = await db.collection('server').get();
-  console.log( querySnapshot.docs.map(doc => ({
-    id: doc.id
-  })) );
-}
-get_firestore();
-*/
+const fireDragon = new Promise((resolve, reject) => {
+  console.log('Do Fire!');
+  // Just testing hérna:
+  setTimeout(() => { resolve("Fire Successful!");}, 300);
+});
+
+
+
+const fireDoc = db.collection('fire');
+const unsub = fireDoc.onSnapshot(docSnapshot => {
+  // Callback function.
+  console.log('Received fireDoc snapshot.');
+  const batch = db.batch();
+
+  docSnapshot.forEach(function(doc){
+    const fireID = doc.id;
+    const code = doc.data().code;
+    const age_sec = docSnapshot.readTime.seconds - doc.data().timestamp.seconds;
+
+    if(age_sec < 45){
+      console.log('Fire ID:', fireID);
+
+      fireDragon.then((result) => {
+        console.log(result);
+        db.collection('permits').doc(code).delete();
+      }).catch((error) => {
+        console.error(error);
+      });
+
+    }
+    batch.delete(doc.ref);
+  });
+  batch.commit();
+}, err => {
+  console.error(`Encountered error: ${err}`);
+});
